9e42fc13240ee0e9ef29c5c0f08fe2cf0d88d96d
weizhouUMICH <zhowei@umich.edu>
2017-11-04 18:48:27 -0400

    Updated example data by adding VCF files and changed the output option from SPAGMMAToutputFile to SAIGEOutputFile

--- extdata/cmd.sh
+++ extdata/cmd.sh
@@ -1,4 +1,5 @@
-##step 1: fit the NULL GLMM
+
+#step 1: fit the NULL GLMM
 Rscript step1_fitNULLGLMM.R \
 	--plinkFile=./input/plinkforGRM_1000samples_10kMarkers \
 	--phenoFile=./input/pheno_1000samples.txt \
@@ -9,8 +10,8 @@ Rscript step1_fitNULLGLMM.R \
 	--outputPrefix=./output/example \
 	--nThreads=4
 
-##step 2: perfrom score test with SPA applied for each marker
-Rscript step2_SPATests.R \
+##step 2: perfrom score test with SPA applied for each marker with the plain text dosage file 
+Rscript step2_SPATests_dosage_plain.R \
 	--dosageFile=./input/dosage_10markers.txt \
 	--dosageFileNrowSkip=1 \
 	--dosageFileNcolSkip=6 \
@@ -23,4 +24,44 @@ Rscript step2_SPATests.R \
         --sampleIDColinphenoFile=IID \
 	--GMMATmodelFile=./output/example.rda \
 	--varianceRatioFile=./output/example.varianceRatio.txt \
-	--SPAGMMAToutputFile=./output/example.SAIGE.txt
+	--SAIGEOutputFile=./output/example.SAIGE.txt \
+	--numLinesOutput=2 \
+        --IsOutputAFinCaseCtrl=TRUE
+
+#step 2: perfrom score test with SPA applied for each marker with the plain text vcf file
+Rscript step2_SPATests_dosage_vcf.R \
+	--vcfFile=./input/dosage_10markers.vcf.gz \
+	--vcfFileIndex=./input/dosage_10markers.vcf.gz.tbi \
+	--vcfField=DS \
+	--chrom=1 \
+	--minMAF=0.0001 \
+	--minMAC=1 \
+	--sampleFile=./input/sampleIDindosage.txt \
+        --phenoFile=./input/pheno_1000samples.txt \
+        --phenoCol=y \
+        --covarColList=X2,X3 \
+        --sampleIDColinphenoFile=IID \
+        --GMMATmodelFile=./output/example.rda \
+        --varianceRatioFile=./output/example.varianceRatio.txt \
+        --SAIGEOutputFile=./output/example.SAIGE.vcf.dosage.txt \
+	--numLinesOutput=2 \
+        --IsOutputAFinCaseCtrl=TRUE	
+
+Rscript step2_SPATests_dosage_vcf.R \
+        --vcfFile=./input/genotype_10markers.vcf.gz \
+        --vcfFileIndex=./input/genotype_10markers.vcf.gz.tbi \
+        --vcfField=GT \
+        --chrom=1 \
+        --minMAF=0.0001 \
+        --minMAC=1 \
+        --sampleFile=./input/sampleIDindosage.txt \
+        --phenoFile=./input/pheno_1000samples.txt \
+        --phenoCol=y \
+        --covarColList=X2,X3 \
+        --sampleIDColinphenoFile=IID \
+        --GMMATmodelFile=./output/example.rda \
+        --varianceRatioFile=./output/example.varianceRatio.txt \
+        --SAIGEOutputFile=./output/example.SAIGE.vcf.genotype.txt \
+        --numLinesOutput=2 \
+        --IsOutputAFinCaseCtrl=TRUE
+
