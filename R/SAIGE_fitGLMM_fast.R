34166760acb5166f71933173a9b5d5160025e52e
weizhouUMICH <zhowei@umich.edu>
2019-01-27 15:18:35 -0500

    reduce null model size by removing hidden environments by Juha

--- R/SAIGE_fitGLMM_fast.R
+++ R/SAIGE_fitGLMM_fast.R
@@ -934,6 +934,11 @@ fitNULLGLMM = function(plinkFile = "",
       system.time(modglmm<-glmmkin.ai_PCG_Rcpp_Binary(plinkFile, fit0, tau = c(0,0), fixtau = c(0,0), maxiter =maxiter, tol = tol, verbose = TRUE, nrun=30, tolPCG = tolPCG, maxiterPCG = maxiterPCG, subPheno = dataMerge_sort, obj.noK = obj.noK, out.transform = out.transform, tauInit=tauInit, memoryChunk=memoryChunk, LOCO=LOCO, chromosomeStartIndexVec = chromosomeStartIndexVec, chromosomeEndIndexVec = chromosomeEndIndexVec, traceCVcutoff = traceCVcutoff, isCovariateTransform = isCovariateTransform, isDiagofKinSetAsOne = isDiagofKinSetAsOne))
      #print("test memory 5")
      #gc(verbose=T)
+      modglmm$obj.glm.null$model <- data.frame(modglmm$obj.glm.null$model)
+      for (x in names(modglmm$obj.glm.null)) {
+        attr(modglmm$obj.glm.null[[x]], ".Environment") <- c()
+      }
+
       save(modglmm, file = modelOut)
 
       t_end = proc.time()
@@ -1080,6 +1085,12 @@ fitNULLGLMM = function(plinkFile = "",
       print(t_begin)
 
       system.time(modglmm<-glmmkin.ai_PCG_Rcpp_Quantitative(plinkFile,fit0, tau = c(0,0), fixtau = c(0,0), maxiter =maxiter, tol = tol, verbose = TRUE, nrun=30, tolPCG = tolPCG, maxiterPCG = maxiterPCG, subPheno = dataMerge_sort, obj.noK=obj.noK, out.transform=out.transform, tauInit=tauInit, memoryChunk = memoryChunk, LOCO=LOCO, chromosomeStartIndexVec = chromosomeStartIndexVec, chromosomeEndIndexVec = chromosomeEndIndexVec, traceCVcutoff = traceCVcutoff, isCovariateTransform = isCovariateTransform, isDiagofKinSetAsOne = isDiagofKinSetAsOne))
+      
+      modglmm$obj.glm.null$model <- data.frame(modglmm$obj.glm.null$model)
+      for (x in names(modglmm$obj.glm.null)) {
+        attr(modglmm$obj.glm.null[[x]], ".Environment") <- c()
+      }
+
       save(modglmm, file = modelOut)
 
       t_end = proc.time()
