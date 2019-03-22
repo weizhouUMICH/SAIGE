188af0d2f588843116d0dfff01c16153b4bfe8db
weizhouUMICH <zhowei@umich.edu>
2019-03-22 13:57:47 -0400

    script to isntall required R packages

---
+++ extdata/install_packages.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+#install required R packages, from Finnge/SAIGE-IT
+
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest")
+for (pack in req_packages) {
+    if(!require(pack,character.only = TRUE)) {
+        install.packages(pack, repos = "http://cran.us.r-project.org")
+    }
+}
