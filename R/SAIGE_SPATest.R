af80de0314ed246cbc0aeb85157daaad8521d5a5
weizhouUMICH <zhowei@umich.edu>
2019-03-24 00:17:19 -0400

    add more detailed warining note for saige-gene for binary traits

--- R/SAIGE_SPATest.R
+++ R/SAIGE_SPATest.R
@@ -870,7 +870,7 @@ SPAGMMATtest = function(dosageFile = "",
        cat("It is a quantitative trait\n")
      }else if(traitType == "binary"){
        cat("It is a binary trait\n")
-       cat("WARNING!!!! Gene-based tests do work for binary traits with unbalanced case-control ratios (disease prevalence < 20%)! \n")	
+       cat("WARNING!!!! Gene-based tests do not work for binary traits with unbalanced case-control ratios (disease prevalence < 20%)! \n")	
      }
 
 
