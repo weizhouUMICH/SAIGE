b491b967909fb50e74de36927faa70e62f307ad2
weizhouUMICH <zhowei@umich.edu>
2019-06-30 00:58:07 -0400

    add SKAT and MetaSKAT in install_packages.R

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 #install required R packages, from Finnge/SAIGE-IT
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "SKAT","MetaSKAT")
 for (pack in req_packages) {
     if(!require(pack,character.only = TRUE)) {
         install.packages(pack, repos = "http://cran.us.r-project.org")
