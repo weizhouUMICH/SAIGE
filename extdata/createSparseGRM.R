2a7a0a0d71ada6cb11baf4d175e3144faa1525b2
Wei Zhou <zhowei@umich.edu>
2020-02-26 06:43:20 +0000

    lib.loc change

--- extdata/createSparseGRM.R
+++ extdata/createSparseGRM.R
@@ -3,7 +3,7 @@
 options(stringsAsFactors=F)
 
 ## load R libraries
-library(SAIGE, lib.loc="../../install_dir/burdenbeta_test/")
+library(SAIGE)
 require(optparse) #install.packages("optparse")
 
 print(sessionInfo())
