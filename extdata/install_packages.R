99f62546c7a18e804e22803241f8a6d46672c999
Wei Zhou <zhowei@umich.edu>
2020-02-26 04:58:32 +0000

    install SKAT from github

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
 #install required R packages, from Finnge/SAIGE-IT
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "SKAT", "roxygen2", "rversions","devtools")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "roxygen2", "rversions","devtools")
 for (pack in req_packages) {
     if(!require(pack, character.only = TRUE)) {
         install.packages(pack, repos = "https://cloud.r-project.org")
     }
 }
+
+devtools::install_github("leeshawn/SKAT")
