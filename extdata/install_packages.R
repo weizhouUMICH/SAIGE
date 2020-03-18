a08ceb0ba7fd287bc5a478c41350633b1cfcfb67
Wei Zhou <zhowei@umich.edu>
2020-03-18 20:41:39 +0000

    SKAT on cran has been updated so it can be installed directly

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 #install required R packages, from Finnge/SAIGE-IT
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "roxygen2", "rversions","devtools")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "roxygen2", "rversions","devtools", "SKAT")
 for (pack in req_packages) {
     if(!require(pack, character.only = TRUE)) {
         install.packages(pack, repos = "https://cloud.r-project.org")
     }
 }
 
-devtools::install_github("leeshawn/SKAT")
+#devtools::install_github("leeshawn/SKAT")
