ae654373522e8999d2db921fe5c6c42c59721f0c
lee7801 <lee7801@lee7801s-iMac.local>
2021-11-05 09:35:07 +0900

    remove third party

--- thirdParty/bgen/test/functional/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dir=$(dirname ${0})
-basedir=${dir}/../..
-echo ${dir}
-pybot -o  output.html -l log.html -r NONE -d ${basedir}/build/test/functional/test-reports -x xunit.xml ${dir}/tests.txt
