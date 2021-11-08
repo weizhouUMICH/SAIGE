59fd17a3f192f1bf756edf63755e6f9d1ce49b4a
Alex Petty <pettyalex@gmail.com>
2021-11-08 15:40:38 -0600

    Use a library from CRAN to limit BLAS threads in Step2, which can only effectively use one single thread

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -2,7 +2,7 @@
 #install required R packages, from Finnge/SAIGE-IT
 
 #req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "roxygen2", "rversions","devtools", "SKAT")
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "roxygen2", "rversions","devtools", "SKAT")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "roxygen2", "rversions","devtools", "SKAT", "RhpcBLASctl")
 for (pack in req_packages) {
     if(!require(pack, character.only = TRUE)) {
         install.packages(pack, repos = "https://cloud.r-project.org")
